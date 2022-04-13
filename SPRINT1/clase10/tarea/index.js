60570cfdb3244f2cc31bb6b36a5008f6d96d84fd
Daniel Mayo <daniielmayo.mayo@gmail.com>
2022-04-13 13:43:47 -0500

    se ha agragado response en la pag personal tarea 10 sprint 1

---
+++ SPRINT1/clase10/tarea/index.js
@@ -0,0 +1,19 @@
+const enviar = document.getElementById("form");
+
+function peliculas() {
+  alert("Estas son mis PELICULAS FAVORITAS");
+}
+function anime() {
+  alert("Estos son mis ANIMES FAVORITOS");
+}
+
+function libros() {
+  alert("Espero que te ayuden estos son mis librios favoritos");
+}
+
+function hobbies() {
+  alert("Esto realizo en mi tiempo libre");
+}
+function forumulario() {
+  alert("Gracias, por tu aporte");
+}
