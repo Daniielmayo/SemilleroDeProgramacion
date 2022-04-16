60278d5337cdc95b56371b6753b07c93860b55cb
Daniel Mayo <daniielmayo.mayo@gmail.com>
2022-04-16 15:23:01 -0500

    correccion de la tarea #3 del sprint 2

--- SPRINT2/Clase#3/tarea/index2.js
+++ SPRINT2/Clase#3/tarea/index2.js
@@ -1,20 +1,18 @@
-// const peliculasFavoritas=[
-//     "Rapidos y furiosos",
-//     "Harry Potter",
-//     "La era del Hielo"
-// ]
-// console.log(`mis peliculas favoritas son: ${peliculasFavoritas.toString()}`);
+
 
 
 const interesesPersonales =[
-    peliculasFavoritas =["Rapidos y Furiosos", "Harry Potter","La Era del Hielo"],
-    animesFavoritos = ["Naruto","Demon Slayer","Death Note","Black Clover"],
-    librosFavoritos = ["Biblia","Como ganar amigos e influir sobre las personas", "Cuentos de los Hermanos Grimm"],
-    hobbies = ["Batallas de freestyle","Jugar Futbol"]
+  peliculasFavoritas =["Rapidos y Furiosos", "Harry Potter","La Era del Hielo"],
+  animesFavoritos = ["Naruto","Demon Slayer","Death Note","Black Clover"],
+  librosFavoritos = ["Biblia","Como ganar amigos e influir sobre las personas", "Cuentos de los Hermanos Grimm"],
+  hobbies = ["Batallas de freestyle","Jugar Futbol"]
 ]
 
 
-  console.log( `Mis peliculas favoritas son : ${peliculasFavoritas}`); 
-  console.log( `Mis animes favoritas son : ${animesFavoritos}`);
-  console.log( `Mis libros favoritas son : ${librosFavoritos}`);
-  console.log( `Mis hobbies favoritas son : ${hobbies}`);
+console.log( `Mis peliculas favoritas son : ${peliculasFavoritas}`); 
+console.log( `Mis animes favoritas son : ${animesFavoritos}`);
+console.log( `Mis libros favoritas son : ${librosFavoritos}`);
+console.log( `Mis hobbies favoritas son : ${hobbies}`);
+
+
+ 
