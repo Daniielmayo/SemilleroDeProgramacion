f86a73de341de295307ae5977e18f69acf79658a
Daniel Mayo <daniielmayo.mayo@gmail.com>
2022-04-28 23:05:54 -0500

    solucion del minireto sprint2

--- SPRINT2/miniRetoSprint2/hotel.js
+++ SPRINT2/miniRetoSprint2/hotel.js
@@ -1,31 +1,35 @@
 import { hotelsData } from "./resources/data.js";
-const hotelsImage = document.getElementById("hotelsImage");
-// console.log(hotelsData)
-console.log(hotelsImage)
+const hotelsImage = document.getElementById("hotelsImage")
+const informacionHotel = document.getElementById("informacionHotel")
+//  console.log(informacionHotel)
+
 const cardHotel = document.createElement("div");
   cardHotel.setAttribute("class","card");
 
     
 
+  function contenido(e) {
+      
+      console.log(e.description)
+   }
+
  hotelsData.forEach((e) => {
      const cardHotel = document.createElement("div");
      cardHotel.setAttribute("class","card");
     
-     console.log(cardHotel);
 
      const imageHotel = document.createElement("img");
      imageHotel.setAttribute("class","card__img");
      imageHotel.setAttribute("src", e.photo);
-     console.log(e.photo);
+     
      imageHotel.setAttribute("alt", "imagen del hotel " + e.name);
+     imageHotel.addEventListener("click", ()=>contenido(e));
 
   
      hotelsImage.appendChild(cardHotel);
      cardHotel.appendChild(imageHotel);
  });
 
-
-  
 
 
 
